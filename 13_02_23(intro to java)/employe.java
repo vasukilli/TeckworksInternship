ab477d6bda2b971bb9190c4d132c46d1630ae11f
vasukilli <124597600+vasukilli@users.noreply.github.com>
2023-02-14 09:59:30 +0530

    Add files via upload

---
+++ 13_02_23(intro to java)/employe.java
@@ -0,0 +1,16 @@
+class employe
+{
+    public static void main(String[] args)
+
+    {
+      string employe_name ="Vasu";
+      int employe_id = 101;
+      int salary = 30,000;
+      string address = "vijayawada";
+      int employe_age = 21; 
+      string gender = "male"
+      char grade = 'A';
+      long phn_num = 6301725926;
+    }
+
+}
---
+++ 13_02_23(intro to java)/student.java
@@ -0,0 +1,14 @@
+class student
+{
+    public static void main(String[] args)
+    {
+     int student_id =548;
+     string student_name = "VASU";
+     int student_age = 21;
+     int student_marks = 600;
+     sting student_address = "VIJAYAEADA";
+     char grade = 'A';
+     int student_fee = 50,000;
+     long phn_num = 6301725926;
+    }
+}
